39afc1ccae09d8c7bbe46fc5622422a46e595058
javabyraghu <javabyraghu@gmail.com>
2021-07-11 00:28:43 +0530

    First Commit

---
+++ lib/src/main/java/in/nareshit/raghu/WelcomeServlet.java
@@ -0,0 +1,26 @@
+package in.nareshit.raghu;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/welcome")
+public class WelcomeServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(
+			HttpServletRequest req, 
+			HttpServletResponse resp)
+			throws ServletException, IOException
+	{
+	
+		PrintWriter out = resp.getWriter();
+		out.print("<h3>WELCOME TO GRADLE APP</h3>");
+	}
+}
